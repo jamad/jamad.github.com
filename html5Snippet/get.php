9247be8c0a606dfe15f012820384de247abc8e9c
jamad <jamad2001@gmail.com>
2011-09-22 22:30:40 +0200

    case name was updated

--- html5Snippet/get.php
+++ html5Snippet/get.php
@@ -4,19 +4,12 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Origin: http://echo.html5snippet.net');
 
-
-
-
 if (isset($_GET['file']))
 {
     switch($_GET['file'])
 	{
-		case 'texture.jpg':
-			header('Content-Type: image/jpg');
-			die (file_get_contents("texture.jpg"));
-		break;
-		
-		case 'model.js':
+
+		case 'blenderMonkey3.js':
 			die (file_get_contents("blenderMonkey3.js"));
 		break;
 	}
