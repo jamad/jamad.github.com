14a04e7f68fe195a496cd80f2a1e9fc173fb5036
jamad <jamad2001@gmail.com>
2011-09-22 22:10:51 +0200

    php file added

---
+++ html5Snippet/get.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Origin: http://echo.html5snippet.net');
+
+
+if (isset($_GET['file']))
+{
+    switch($_GET['file'])
+	{
+		case 'texture.jpg':
+			header('Content-Type: image/jpg');
+			die (file_get_contents("texture.jpg"));
+		break;
+		
+		case 'model.js':
+			die (file_get_contents("model.js"));
+		break;
+	}
+}
